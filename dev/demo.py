e79f04a1b6bb7b2f2f8fa5be8f0c26022f9e55d9
brett-schneider <github@cbret.de>
2020-06-27 14:38:39 +0200

    .

--- dev/demo.py
+++ dev/demo.py
@@ -18,7 +18,18 @@ def getmaxcharge(): #dummy for maximum chargeability
     return 20
 
 token = "0xce4b48DF1E88DFd74da1963416a53bBA9cf3B2aE"
-charger = "0x961D954009Db8D9ab527632D7537411f3b3b8473"
+#charger = "0x961D954009Db8D9ab527632D7537411f3b3b8473" # node 3
+charger = "0xE76Ba8dA05B3fdA938Cd76fC4A9D044d0ab45Cd9" # node 6
+priceperkwh = 300000000000000000 # 0.3 EBC
+factor = 1000000000000000000
+
+def pget(url):
+    print ('{}/{}'.format(endpoint,url))
+    r = requests.get('{}/{}'.format(endpoint,url))
+    print (r.status_code)
+    d = json.loads(r.text)
+    for p in d:
+        print (p)
 
 def pay(token, target, amount):
     r = requests.post('{}/payments/{}/{}'.format(endpoint,token,target), 
@@ -26,12 +37,25 @@ def pay(token, target, amount):
         json={ 'amount': '{}'.format(amount), 'identifier': '1', })
     print (r.status_code)
     print (r.text)
-    #if r.status_code == 409:   # 409 conflict: If the address or the amount is invalid or if there is no path to the target, or if the identifier is already in use for a different payment.
+    if r.status_code == 409:   # 409 conflict: If the address or the amount is invalid or if there is no path to the target, or if the identifier is already in use for a different payment.
+        r = requests.put('{}/channels', 
+            headers={ 'Content-Type': 'application/json', }, 
+            json={ 'partner_address': target
+                , 'reveal_timeout': '50'
+                , 'settle_timeout': '500'
+                , 'token_address': token
+                , 'total_deposit': '35000000', })
+        print (r.status_code)
+        print (r.text)
+    
     # 200 OK
     # 404 Not Found: The given token and / or target addresses are not valid eip55-encoded Ethereum addresses
     # 402 Payment Required: If the payment can’t start due to insufficient balance
     # 400 Bad Request: malformed json
 
+#pget ('channels')
+pget ('channels/{}'.format(token))
+exit (0)
 
 start = time.time()
 pay (token, charger, 1)
