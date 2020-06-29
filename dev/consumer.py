e1f377e3f0026cc0ce53bcf4dddb7432bad85dbf
Mateusz Piotrowski <m.piotrowski@campus.tu-berlin.de>
2020-06-29 13:22:09 +0200

    Lint Python scripts with Black
    https://github.com/psf/black

--- dev/consumer.py
+++ dev/consumer.py
@@ -1,6 +1,6 @@
 #!/usr/local/opt/python@3.8/bin/python3
 
-# geth --http --goerli --http.corsdomain="*" 
+# geth --http --goerli --http.corsdomain="*"
 
 import json
 import time
@@ -10,41 +10,47 @@ from raiden_api import rnode
 
 node = rnode(5001)
 
-def getmaxcharge(): #dummy for maximum chargeability
+
+def getmaxcharge():  # dummy for maximum chargeability
     return 20
 
+
 token = "0xce4b48DF1E88DFd74da1963416a53bBA9cf3B2aE"
-charger = "0x961D954009Db8D9ab527632D7537411f3b3b8473" # node 3
-#charger = "0xE76Ba8dA05B3fdA938Cd76fC4A9D044d0ab45Cd9" # node 6
-priceperkwh = 300000000000000000 # 0.3 EBC
+charger = "0x961D954009Db8D9ab527632D7537411f3b3b8473"  # node 3
+# charger = "0xE76Ba8dA05B3fdA938Cd76fC4A9D044d0ab45Cd9" # node 6
+priceperkwh = 300000000000000000  # 0.3 EBC
 factor = 1000000000000000000
-    
+
 q = node.histpay()
 payhist = json.loads(q.text)
 # print (type(payhist))
 
 for f in payhist:
-    if 'identifier' not in f:
-        f['identifier'] = '0'
-    if 'amount' not in f:
-        f['amount'] = '0'
+    if "identifier" not in f:
+        f["identifier"] = "0"
+    if "amount" not in f:
+        f["amount"] = "0"
     # print (f)
-    print ('{}: {} at {} :: {}'.format(f['identifier'],f['amount'],f['log_time'],f['event']))
+    print(
+        "{}: {} at {} :: {}".format(
+            f["identifier"], f["amount"], f["log_time"], f["event"]
+        )
+    )
     # print (datetime.strptime(f['log_time'],'%Y-%m-%dT%H:%M:%S.%f'))
 
 
-payid = random.randrange(1,1000000000)
+payid = random.randrange(1, 1000000000)
 while True:
     try:
         start = time.time()
-        #s = node.pay(charger,1,node.token,counter)
-        s = node.pay(charger,1,node.token,payid)
+        # s = node.pay(charger,1,node.token,counter)
+        s = node.pay(charger, 1, node.token, payid)
         end = time.time()
-        print (end - start)
+        print(end - start)
         # 409 conflict: If the address or the amount is invalid or if there is no path to the target, or if the identifier is already in use for a different payment.
-        if s.status_code == 409:   
-            print ('409 encountered, opening channel')
-            o = node.openchan(charger,getmaxcharge*priceperkwh)
+        if s.status_code == 409:
+            print("409 encountered, opening channel")
+            o = node.openchan(charger, getmaxcharge * priceperkwh)
             if o.status_code != 201:
                 break
     except KeyboardInterrupt:
@@ -55,16 +61,17 @@ while True:
     # 402 Payment Required: If the payment can’t start due to insufficient balance
     # 400 Bad Request: malformed json
 
-exit (0)
+exit(0)
 
 start = time.time()
-node.pay (charger,1,node.token,100)
+node.pay(charger, 1, node.token, 100)
 end = time.time()
-print (end - start)
+print(end - start)
 
 # some reactive stuff i'm working on
 from rx3 import create
 from time import sleep
+
 
 def runmeter(observer, scheduler):
     try:
@@ -74,12 +81,13 @@ def runmeter(observer, scheduler):
     except KeyboardInterrupt:
         observer.on_completed()
 
-exit (0)    
+
+exit(0)
 
 source = create(runmeter)
 
 source.subscribe(
-    on_next = lambda i: print("Received {0}".format(i)),
-    on_error = lambda e: print("Error Occurred: {0}".format(e)),
-    on_completed = lambda: print("Done!"),
+    on_next=lambda i: print("Received {0}".format(i)),
+    on_error=lambda e: print("Error Occurred: {0}".format(e)),
+    on_completed=lambda: print("Done!"),
 )
--- dev/raiden_api.py
+++ dev/raiden_api.py
@@ -1,90 +1,99 @@
 #!/usr/local/opt/python@3.8/bin/python3
 
-# geth --http --goerli --http.corsdomain="*" 
+# geth --http --goerli --http.corsdomain="*"
 
 import requests
 import json
 import time
 import random
 
-class rnode:
-    def dbg(self,**kwargs):
-        if self.dbglevel > 0:
-            print (**kwargs)
 
-    def __init__(self,port,endpoint = None,token = None):
+class rnode:
+    def dbg(self, **kwargs):
+        if self.dbglevel > 0:
+            print(**kwargs)
+
+    def __init__(self, port, endpoint=None, token=None):
         self.port = port
         if endpoint is not None:
             self.endpoint = endpoint
         else:
-            self.endpoint = endpoint = 'http://localhost:{}/api/v1'.format(port)
-        r = requests.get('{}/address'.format(endpoint))
+            self.endpoint = endpoint = "http://localhost:{}/api/v1".format(port)
+        r = requests.get("{}/address".format(endpoint))
         d = json.loads(r.text)
-        self.address = d['our_address']
+        self.address = d["our_address"]
         self.token = token
         if token is None:
             self.token = "0xce4b48DF1E88DFd74da1963416a53bBA9cf3B2aE"
 
-    def pget(self,url):
-        print ('{}/{}'.format(self.endpoint,url))
-        r = requests.get('{}/{}'.format(self.endpoint,url))
-        print (r.status_code)
+    def pget(self, url):
+        print("{}/{}".format(self.endpoint, url))
+        r = requests.get("{}/{}".format(self.endpoint, url))
+        print(r.status_code)
         d = json.loads(r.text)
         for p in d:
-            print (p)
+            print(p)
 
-    def pay(self,target,amount,token = None,id = 1):
+    def pay(self, target, amount, token=None, id=1):
         ptoken = token
         if ptoken is None:
             ptoken = self.token
-        print ('{}/payments/{}/{}'.format(self.endpoint,ptoken,target))
-        #print ({ 'amount': '{}'.format(amount), 'identifier': '{}'.format(id), })
-        print ({ 'amount': amount, 'identifier': id, })
-        r = requests.post('{}/payments/{}/{}'.format(self.endpoint,ptoken,target), 
-            headers={ 'Content-Type': 'application/json', }, 
-            json={ 'amount': amount, 'identifier': id, })
-        print (r.status_code)
-        print (r.text)
+        print("{}/payments/{}/{}".format(self.endpoint, ptoken, target))
+        # print ({ 'amount': '{}'.format(amount), 'identifier': '{}'.format(id), })
+        print(
+            {"amount": amount, "identifier": id,}
+        )
+        r = requests.post(
+            "{}/payments/{}/{}".format(self.endpoint, ptoken, target),
+            headers={"Content-Type": "application/json",},
+            json={"amount": amount, "identifier": id,},
+        )
+        print(r.status_code)
+        print(r.text)
         return r
 
-    def openchan(self,target,deposit,token = None):
+    def openchan(self, target, deposit, token=None):
         ptoken = token
         if ptoken is None:
             ptoken = self.token
-        c = requests.put('{}/channels'.format(self.endpoint,ptoken,target), 
-            headers={ 'Content-Type': 'application/json', }, 
-            json={ 'partner_address': target
-                , 'reveal_timeout': '50'
-                , 'settle_timeout': '500'
-                , 'token_address': ptoken
-                , 'total_deposit': deposit, })
-        print (c.status_code)
-        print (c.text)
+        c = requests.put(
+            "{}/channels".format(self.endpoint, ptoken, target),
+            headers={"Content-Type": "application/json",},
+            json={
+                "partner_address": target,
+                "reveal_timeout": "50",
+                "settle_timeout": "500",
+                "token_address": ptoken,
+                "total_deposit": deposit,
+            },
+        )
+        print(c.status_code)
+        print(c.text)
         return c
 
-    def histpay(self,token = None,target = None,id = None):
-        # Query the payment history. This includes successful (EventPaymentSentSuccess) and 
-        # failed (EventPaymentSentFailed) sent payments as well as received payments (EventPaymentReceivedSuccess). 
+    def histpay(self, token=None, target=None, id=None):
+        # Query the payment history. This includes successful (EventPaymentSentSuccess) and
+        # failed (EventPaymentSentFailed) sent payments as well as received payments (EventPaymentReceivedSuccess).
         # token_address and target_address are optional and will filter the list of events accordingly.
         ptoken = token
         if ptoken is None:
             ptoken = self.token
-        print ('target: {}'.format(target))
-        ptarget = '/{}'.format(target)
+        print("target: {}".format(target))
+        ptarget = "/{}".format(target)
         if target is None:
-            ptarget = ''
-        print ('{}/payments/{}{}'.format(self.endpoint,ptoken,ptarget))
-        r = requests.get('{}/payments/{}{}'.format(self.endpoint,ptoken,ptarget))
+            ptarget = ""
+        print("{}/payments/{}{}".format(self.endpoint, ptoken, ptarget))
+        r = requests.get("{}/payments/{}{}".format(self.endpoint, ptoken, ptarget))
         # print (r.status_code)
         # print (r.text)
         # self.dbg (r.status_code)
         # self.dbg (r.text)
         return r
 
-    def registertoken(self,token = None):
-        r = requests.put('{}/tokens/{}'.format(self.endpoint,token))
+    def registertoken(self, token=None):
+        r = requests.put("{}/tokens/{}".format(self.endpoint, token))
         return r
-    
-    def listchan(self,token = None,target = None):
+
+    def listchan(self, token=None, target=None):
         # no defaulting, because none -> all channels for all tokens
-        return pget(self,'/channels/{}/{}'.format(token,target))
+        return pget(self, "/channels/{}/{}".format(token, target))
